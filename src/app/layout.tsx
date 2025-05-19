60a1fadabc27b267493df86657e60cb0df398d1c
dantakelly <cultertraz@gmail.com>
2025-05-19 14:10:59 -0500

    added some vercel packages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
-// import Header from "@/components/header/header";
 import Header from "../components/header/header";
-// import Footer from "@/components/footer/footer";
 import Footer from "../components/footer/footer";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import { SessionProvider } from "next-auth/react";
 
